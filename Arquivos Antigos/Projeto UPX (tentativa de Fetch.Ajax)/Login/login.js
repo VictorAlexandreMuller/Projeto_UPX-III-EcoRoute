6e73e0fe058a79a3f085a208299eef6ad6cbfe12
VictorAlexandreMuller <victor.alexandre.mu@gmail.com>
2024-04-29 14:30:19 -0300

    just change principal folder

--- Arquivos Antigos/Projeto UPX (tentativa de Fetch.Ajax)/Login/login.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var entrarBtn = document.getElementById("entrar-btn");
-
-  entrarBtn.addEventListener("click", function () {
-    window.location.href = "../PósLogin/poslogin.html";
-  });
-});
